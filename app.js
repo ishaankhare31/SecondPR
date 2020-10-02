5830336f7409c9044d3cbd22d3aca3674c48c708
ishaankhare31 <54032725+ishaankhare31@users.noreply.github.com>
2020-10-02 15:44:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,22 @@ app.use('/leaders', leaderRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+from django import forms
+from django.contrib.auth.models import User
+from basic_app.models import UserProfileInfo
 
+
+class UserForm(forms.ModelForm):
+    password= forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username','email','password')
+
+class UserProfileInfoForm(forms.ModelForm):
+    class Meta():
+        model=UserProfileInfo
+        fields = ('portfolio_site','profile_pic')
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
